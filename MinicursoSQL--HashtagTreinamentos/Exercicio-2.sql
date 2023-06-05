b491a0bef3d53c42a7101b2e249e916e3b6fbf8c
Walter J. Silva <walter4web@gmail.com>
2023-06-05 16:34:34 -0300

    Add exercise 2 command

---
+++ MinicursoSQL--HashtagTreinamentos/Exercicio-2.sql
@@ -0,0 +1,16 @@
+/* EXERCÍCIO 2 */
+
+/*
+a) Faça uma consulta na tabela de lojas e ordene a tabela de acordo com a coluna de 
+Num_Funcionarios, em ordem crescente.
+Obs: Sua consulta deve retornar as colunas de Loja (AS 'Nome da Loja'), Endereco (AS
+'Endereço'), Num_Funcionarios (AS 'Qtd. Funcionários') e Telefone (AS 'Número para contato').
+*/
+
+
+
+
+/*
+b) Faça uma cosulta na tabela de clientes, ordenando a tabela de acordo com o nome
+(ordem alfabética) e sobrenome (ordem alfabética). Retorne todas as colunas e linhas da tabela.
+*/
