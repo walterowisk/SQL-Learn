f2d09cdc5d9c5be6f31cb052264415e35feea2f3
Walter J. Silva <walter4web@gmail.com>
2023-06-05 16:58:47 -0300

    Add exercise 2 solved

--- MinicursoSQL--HashtagTreinamentos/Exercicio-2.sql
+++ MinicursoSQL--HashtagTreinamentos/Exercicio-2.sql
@@ -7,10 +7,17 @@ Obs: Sua consulta deve retornar as colunas de Loja (AS 'Nome da Loja'), Endereco
 'Endereço'), Num_Funcionarios (AS 'Qtd. Funcionários') e Telefone (AS 'Número para contato').
 */
 
-
-
+SELECT Loja AS 'Nome da Loja',
+       Endereco AS 'Endereço',
+       Num_Funcionarios AS 'Qtd. Funcionários',
+       Telefone AS 'Número para contato'
+FROM lojas
+ORDER BY Num_Funcionarios ASC;
 
 /*
 b) Faça uma cosulta na tabela de clientes, ordenando a tabela de acordo com o nome
 (ordem alfabética) e sobrenome (ordem alfabética). Retorne todas as colunas e linhas da tabela.
 */
+
+SELECT * FROM clientes
+ORDER BY Nome, Sobrenome ASC;
