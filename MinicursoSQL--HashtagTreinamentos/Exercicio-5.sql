6c4601b1575aef7d5b549a30ee10af9f9336b5af
Walter J. Silva <walter4web@gmail.com>
2023-06-06 15:01:36 -0300

    Add exercise 5 solved

--- MinicursoSQL--HashtagTreinamentos/Exercicio-5.sql
+++ MinicursoSQL--HashtagTreinamentos/Exercicio-5.sql
@@ -1,5 +1,34 @@
 /* Exercício 5 */
 
 -- a) Faça um agrupamento para descobrir o total de clientes por Sexo.
+
+SELECT 
+	Sexo, 
+  COUNT(Nome) AS 'Qtd. Clientes'
+FROM 
+	clientes
+GROUP BY Sexo;
+
 -- b) Faça um agrupamento para descobrir quantos produtos existem por categoria.
+
+SELECT 
+	Categoria,
+  COUNT(*) AS 'Qtd. Produtos'
+FROM 
+	produtos
+INNER JOIN categorias
+	ON produtos.ID_Categoria = categorias.ID_Categoria
+GROUP BY Categoria;
+
 -- c) Faça um agrupamento para descobrir a soma total de receita por Loja.
+
+SELECT 
+	Loja,
+  SUM(Receita_Venda) AS 'Receita Total',
+  SUM(Custo_Venda) AS 'Custo Total'
+FROM 
+	pedidos
+INNER JOIN lojas
+	ON pedidos.ID_Loja = lojas.ID_Loja
+GROUP BY Loja;
+	
