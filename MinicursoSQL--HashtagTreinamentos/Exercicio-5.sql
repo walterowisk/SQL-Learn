82afbdcfa641214f95fcbc0c3ef1f82f50fa3fbb
Walter J. Silva <walter4web@gmail.com>
2023-06-06 11:44:36 -0300

    Add exercise 5

---
+++ MinicursoSQL--HashtagTreinamentos/Exercicio-5.sql
@@ -0,0 +1,5 @@
+/* Exercício 5 */
+
+-- a) Faça um agrupamento para descobrir o total de clientes por Sexo.
+-- b) Faça um agrupamento para descobrir quantos produtos existem por categoria.
+-- c) Faça um agrupamento para descobrir a soma total de receita por Loja.
