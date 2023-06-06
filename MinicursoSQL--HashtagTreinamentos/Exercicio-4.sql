d850462caca06dbdf89546135f9bf24783da16bc
Walter J. Silva <walter4web@gmail.com>
2023-06-06 11:41:58 -0300

    Add exercise 4 solved

--- MinicursoSQL--HashtagTreinamentos/Exercicio-4.sql
+++ MinicursoSQL--HashtagTreinamentos/Exercicio-4.sql
@@ -2,14 +2,22 @@
 
 -- a) Descubra o total de custo da tabela de pedidos.
 
-
+SELECT SUM(Custo_Venda) AS 'Custo Total'
+FROM pedidos;
 
 -- b) Descubra a quantidade total de lojas.
 
-
+SELECT COUNT(Loja) AS 'Qtd. Lojas'
+FROM lojas;
 
 -- c) Descubra o custo médio dos produtos (de acordo com a tabela de produtos).
 
-
+SELECT AVG(Custo_Unit) AS 'Custo Médio'
+FROM produtos;
 
 -- d) Descubra a quantidade mínima e máxima de funcionários das lojas (faça isso em um único SELECT).
+
+SELECT
+	MIN(Num_Funcionarios) AS 'Qtd. Mínima',
+	MAX(Num_Funcionarios) AS 'Qtd. Máxima'
+FROM lojas;
