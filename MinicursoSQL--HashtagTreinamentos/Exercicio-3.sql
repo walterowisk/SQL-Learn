9df4c9c904c7b328eef2a8621fba9d0fc2ebb99b
Walter J. Silva <walter4web@gmail.com>
2023-06-06 11:05:21 -0300

    Add exercise 3 solved

--- MinicursoSQL--HashtagTreinamentos/Exercicio-3.sql
+++ MinicursoSQL--HashtagTreinamentos/Exercicio-3.sql
@@ -1,8 +1,11 @@
 /* EXERCÍCIO 3 */
 
--- a) Faça um filtro na tabela de produtos e retorne apenas os produtos com custo maior ou
-igual a 200.
+-- a) Faça um filtro na tabela de produtos e retorne apenas os produtos com custo maior ou igual a 200.
 
-
+SELECT * FROM produtos
+WHERE Custo_Unit >= 200;
 
 -- b) Faça um filtro na tabela de produtos e retorne apenas os produtos da marca "DELL"
+
+SELECT * FROM produtos
+WHERE Marca_Produto = 'DELL';
