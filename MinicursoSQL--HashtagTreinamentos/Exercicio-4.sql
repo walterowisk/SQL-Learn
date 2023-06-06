196b776855a938fa2f469391917e769a4db18924
Walter J. Silva <walter4web@gmail.com>
2023-06-06 11:23:37 -0300

    Add exercise 4

---
+++ MinicursoSQL--HashtagTreinamentos/Exercicio-4.sql
@@ -0,0 +1,15 @@
+/* Exercício 4 */
+
+-- a) Descubra o total de custo da tabela de pedidos.
+
+
+
+-- b) Descubra a quantidade total de lojas.
+
+
+
+-- c) Descubra o custo médio dos produtos (de acordo com a tabela de produtos).
+
+
+
+-- d) Descubra a quantidade mínima e máxima de funcionários das lojas (faça isso em um único SELECT).
