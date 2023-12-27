8d7f706bc4a35d2d4b31aea3f277960a5eb1f748
Walter J. Silva <walter4web@gmail.com>
2023-12-27 16:00:35 -0300

    :sparkles: feat: add question 4

--- SQL-Exercises-level-easy/easy-questions1.sql
+++ SQL-Exercises-level-easy/easy-questions1.sql
@@ -1,15 +1,16 @@
 -- Questão 1: Selecione a tabela de Produtos do banco de dados 'Base' (Minicurso SQL - Hashtag) 
-
 SELECT * FROM produtos;
 
 -- Questão 2: Crie duas consultas nesta mesma janela de query:
 -- A consulta 1 deve retornar a tabela de categorias;
 -- A consulta 2 deve retornar a tabela de lojas.
-
 SELECT * FROM categorias;
-
 SELECT * FROM lojas;
 
 -- Questão 3: Quais marcas de produtos são vendidas pela empresa?
-
 SELECT DISTINCT  Marca_Produto FROM produtos;
+
+-- Questão 4: Quais são os 3 maiores preços de produtos?
+SELECT *  FROM produtos
+ORDER BY Preco_Unit DESC
+LIMIT 3;
